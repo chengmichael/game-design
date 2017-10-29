d1b893ccc8f6f8633a2c5bd8f1f8b52e0fa9f393
Bill Yung <bill.ken.yung@gmail.com>
2017-10-29 15:29:43 -0400

    Respawning objects during pickup

---
+++ Animal Movement Scripts/PickUpDrag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpDrag : MonoBehaviour {
+	float distance = 1;
+	public GameObject obj;
+
+	void OnMouseDown() {
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Instantiate (obj, new Vector3 (0, 2, 0), Quaternion.identity);
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		obj.transform.position = objPosition;﻿
+	}
+}
