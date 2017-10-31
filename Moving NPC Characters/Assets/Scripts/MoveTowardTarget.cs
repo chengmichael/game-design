f3623e512b9796f53ffb1ac0dea5e07151237694
chendonna <chendonna08@gmail.com>
2017-10-31 15:18:15 -0400

    added prototyping project folder

---
+++ Moving NPC Characters/Assets/Scripts/MoveTowardTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardTarget : MonoBehaviour {
+
+    public string targetTag;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.FindGameObjectWithTag(targetTag).transform.position, (speed * 0.1f));
+    }
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.CompareTag ("Despawn"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
